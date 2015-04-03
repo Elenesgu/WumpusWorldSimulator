be78145028a48b46f264b143af45f263e17e6865
SeungminLee <elenesgu@gmail.com>
2015-04-03 14:47:51 +0900

    Make Base System

--- WumpusWorldSimulator/Agent.cpp
+++ WumpusWorldSimulator/Agent.cpp
@@ -1,80 +1,52 @@
 // Agent.cpp
 
 #include <iostream>
+#include <vector>
 #include "Agent.h"
 
 using namespace std;
 
-Agent::Agent ()
-{
+Agent::Agent () {
 
 }
 
-Agent::~Agent ()
-{
+Agent::~Agent () {
 
 }
 
-void Agent::Initialize ()
-{
+void Agent::Initialize () {
 
 }
 
 
-void Agent::Initialize(int worldSize, MapState* map)
-{
-	int x;
-	int y;
-	
-	cout << "Map check\n";
-	cout << "0 = NONE\n" << "1 = PIT\n" << "2 = WUMPUS\n" << "3 = GOLD\n" << "4 = PIT_GOLD\n" << "5 = PIT_WUMPUS\n" << "6 = WUMPUS_GOLD\n";
-	cout << "7 = PIT_WUMPUS_GOLD\n";
-	for (y = worldSize-1; y >= 0; y--)
-	{
-		cout << y << ":	";
-		for (x = 0; x < worldSize; x++)
-		{
-			cout << map[y*worldSize + x] << '\t';
+void Agent::Initialize(int worldSize, MapState* map) {
+	mapdata = MapData(worldSize, std::vector<MapState>(worldSize));
+	for (int row = 0; row < worldSize; row++) {
+		for (int col = 0; col < worldSize; col++) {
+			mapdata[row][col] = *map;
+			map++;
 		}
-		cout << '\n';
 	}
-	
 }
 
 
-Action Agent::Process (Percept& percept)
-{
-	char c;
+Action Agent::Process (Percept& percept) {
 	Action action;
-	bool validAction = false;
-
-	while (! validAction)
-	{
-		validAction = true;
-		cout << "Action? ";
-		cin >> c;
-		if (c == 'f') {
-			action = FORWARD;
-		} else if (c == 'l') {
-			action = TURNLEFT;
-		} else if (c == 'r') {
-			action = TURNRIGHT;
-		} else if (c == 'g') {
-			action = GRAB;
-		} else if (c == 's') {
-			action = SHOOT;
-		} else if (c == 'c') {
-			action = CLIMB;
-		} else {
-			cout << "Huh?" << endl;
-			validAction = false;
-		}
-	}
+	AstarAlgo Algorithm;
+	//TO DO
+	action = Algorithm(percept,mapdata, Heuristic::Zero);
 	return action;
 }
 
-void Agent::GameOver (int score)
-{
+void Agent::GameOver (int score) {
 
 }
 
+
+Action AstarAlgo::operator() (Percept& percepnt, const MapData& mapdata, std::function<int(const MapData&)> HeuristicFunc) {
+	return TURNLEFT;
+}
+
+int Heuristic::Zero(const MapData& mapdata) {
+	return 0;
+}
--- WumpusWorldSimulator/Agent.h
+++ WumpusWorldSimulator/Agent.h
@@ -5,10 +5,14 @@
 
 #include "Action.h"
 #include "Percept.h"
+#include <array>
+#include <functional>
 
+typedef std::vector<std::vector<MapState>> MapData;
 
-class Agent
-{
+class Agent {	
+private:
+	MapData mapdata;
 public:
 	Agent ();
 	~Agent ();
@@ -17,5 +21,13 @@ public:
 	Action Process (Percept& percept);
 	void GameOver (int score);
 };
+
+struct AstarAlgo{
+	Action operator() (Percept& percept, const MapData& mapdata, std::function<int(const MapData&)> HeuristicFunc);
+};
+
+namespace Heuristic {
+	int Zero(const MapData& mapdata);
+}
 
 #endif // AGENT_H
