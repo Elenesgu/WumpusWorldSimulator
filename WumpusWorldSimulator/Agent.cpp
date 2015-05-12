e036f55f92f70bcc50ad7d0e6b9881bf127f19f2
SeungminLee <elenesgu@gmail.com>
2015-05-12 16:05:46 +0900

    옜날에 했던거 갖다 붙임

--- WumpusWorldSimulator/Agent.cpp
+++ WumpusWorldSimulator/Agent.cpp
@@ -5,6 +5,12 @@
 #include <vector>
 #include "Agent.h"
 
+AstarAlgo::mOrient AstarAlgo::table[16] = { N, U, L, R,
+U, N, R, L,
+R, L, N, U,
+L, R, U, N
+};
+
 Coord2::Coord2() : Coord2(0, 0) {
 }
 Coord2::Coord2(int ax, int ay) : x(ax), y(ay) {
@@ -111,6 +117,7 @@ Agent::~Agent () {
 
 void Agent::Initialize() {
 	curPosition = Coord2(0, 0);
+	curOrient = RIGHT;
 	ended = false;
 }
 
@@ -126,22 +133,7 @@ Coord2 Agent::FindNextDest() {
 			}
 		}
 	}
-}
-
-_coordlist Agent::FindPath(const Coord2& dest){
-	Coord2 origin = curPosition;
-	std::vector<std::pair<Coord2, Coord2>> Graph;
-	std::queue<Coord2> nextQueue;
-	nextQueue.push(origin);
-	while (nextQueue.empty()) {
-
-	}
-	_coordlist path;
-	return path;
-}
-
-void Agent::MakeActionQueue(_coordlist& path) {
-
+	return Coord2(-1, -1);
 }
 
 Action Agent::Process (Percept& percept) {
@@ -155,9 +147,6 @@ Action Agent::Process (Percept& percept) {
 		if (percept.Glitter) {
 			action = GRAB;
 			ended = true;
-			_coordlist path = FindPath(Coord2(0, 0));
-			actionQueue.clear();
-			MakeActionQueue(path);
 		}
 		else {
 			KB.GetPercept(curPosition, percept);
@@ -165,7 +154,11 @@ Action Agent::Process (Percept& percept) {
 
 		if (actionQueue.empty()){
 			Coord2 NextDest = FindNextDest();
-			_coordlist path = FindPath(NextDest);
+			if (NextDest.x == -1 || NextDest.y == -1) {
+				//모험을 한다.
+				NextDest = Coord2(0, 0);
+			}
+			actionQueue = algoMethod(KB.mapData, curPosition, NextDest, curOrient);
 		}
 		return action;
 	}
@@ -174,4 +167,215 @@ Action Agent::Process (Percept& percept) {
 void Agent::GameOver (int score) {
 
 }
+#pragma endregion
+
+
+#pragma region A*Algorithm
+
+std::deque<Action> AstarAlgo::operator()(const AstarAlgo::MapData& mapdata,
+	const Coord2& origin, const Coord2& dest, Orientation initori) {
+	try {
+		Compute(mapdata, origin, dest, initori);
+	}
+	catch (bool answer) {
+		if (answer == false) {
+			//길이 없다
+		}
+		else {
+			throw std::exception("코딩 다시해");
+		}
+	}
+	int a = 3;
+	return std::deque<Action>();
+}
+
+void AstarAlgo::Compute(const MapData& mapdata, Coord2 start,
+	Coord2 target, Orientation initorient) {
+
+	isAccessed = std::vector<std::vector<bool>>(mapdata.size(),
+		std::vector<bool>(mapdata.size(), false));
+
+	Graph.clear();
+	Node headNode;
+	headNode.x = start.x;
+	headNode.y = start.y;
+	headNode.parentIndex = -1;
+	headNode.index = 0;
+	headNode.cost = 0;
+	headNode.or = initorient;
+	Graph.push_back(headNode);
+	Compute(mapdata, Graph[0], target);
+}
+
+//Recursive Call for A* algorithm
+//Construct Tree using A* algorithm
+//Tree is stored in AstarAlgo::Graph
+void AstarAlgo::Compute(const MapData& mapdata, const AstarAlgo::Node& curNode, Coord2 target) {
+	isAccessed[curNode.x][curNode.y] = true;
+	Candidates.clear();
+	//Chekc if it is final state.
+	if (curNode.x == target.x && curNode.y == target.y) {
+		return;
+	}
+	for_each(Graph.begin(), Graph.end(), [this, &mapdata](const Node& n) {
+		Node newNode;
+		for (int i = -1; i < 2; i += 2) {
+			//Check adjacent node
+			newNode.x = n.x + i;
+			newNode.y = n.y;
+			newNode.parentIndex = n.index;
+			if (Coord2::CheckValid(Coord2(newNode.x, newNode.y))) {
+				if (isAccessed[newNode.x][newNode.y] == false) {
+					this->Candidates.push_back(newNode);
+				}
+			}
+			newNode.x = n.x;
+			newNode.y = n.y + i;
+			newNode.parentIndex = n.index;
+			if (Coord2::CheckValid(Coord2(newNode.x, newNode.y))) {
+				if (isAccessed[newNode.x][newNode.y] == false) {
+					this->Candidates.push_back(newNode);
+				}
+			}
+		}
+	});
+	Node* minNode = nullptr;
+	long long minCost = upperBound;
+	for_each(Candidates.begin(), Candidates.end(), [this, &minNode, &mapdata, &minCost, &target](Node& n) {
+		n.or = calcOrient(Graph[n.parentIndex], n);
+		n.cost = Graph[n.parentIndex].cost + Heuristic::CostBase(mapdata, Graph, n, target);
+		if (mapdata[n.x][n.y].pitfall || mapdata[n.x][n.y].wumpus) {
+			n.cost += upperBound;
+		}
+		if (n.cost < minCost) {
+			minCost = n.cost;
+			minNode = &n;
+		}
+	});
+	if (minNode == nullptr) {
+		throw false;
+	}
+	minNode->index = Graph.size();
+	Graph.push_back(*minNode);
+	Compute(mapdata, *minNode, target);
+}
+
+void AstarAlgo::MakeAction(const MapData& mapdata) {
+
+	///*mOrient table[16] = {N, U, L, R,
+	//U, N, R, L,
+	//R, L, N, U,
+	//L, R, U, N
+	//};*/
+	////Lambda for caculating action.
+	//auto calcAction = [this, &mapdata](const Node& source, const Node& target, Orientation& curOrient) {
+	//	auto targetOrinet = calcOrient(source, target);
+	//	switch (table[curOrient * 4 + targetOrinet]) {
+	//	case N:
+	//		break;
+	//	case U:
+	//		ActionQue.push(TURNLEFT); ActionQue.push(TURNLEFT); break;
+	//	case L:
+	//		ActionQue.push(TURNLEFT); break;
+	//	case R:
+	//		ActionQue.push(TURNRIGHT); break;
+	//	}
+	//	curOrient = targetOrinet;
+	//};
+	//Compute(mapdata, std::make_pair<int, int>(0, 0), Dest, curOrient);
+	//NodeData Path;
+	//Node cur = Graph.back();
+	//if (cur.x != Dest.first || cur.y != Dest.second) {
+	//	ActionQue.push(CLIMB);
+	//	return;
+	//}
+	//while (cur.parentIndex != -1) {
+	//	Path.push_back(cur);
+	//	cur = Graph[cur.parentIndex];
+	//}
+	//Path.push_back(cur);
+	//for (int index = Path.size() - 2; index >= 0; index--) {
+	//	calcAction(Path[index + 1], Path[index], curOrient);
+	//}
+	//ActionQue.push(GRAB);
+	////For complexity Calc
+	//maxGraphSize = Graph.size();
+	////--------------
+
+	////Back route
+	//Compute(mapdata, Dest, std::make_pair<int, int>(0, 0), curOrient);
+	//Path.clear();
+	//cur = Graph.back();
+	//while (cur.parentIndex != -1) {
+	//	Path.push_back(cur);
+	//	cur = Graph[cur.parentIndex];
+	//}
+	//Path.push_back(cur);
+	//for (int index = Path.size() - 2; index >= 0; index--) {
+	//	calcAction(Path[index + 1], Path[index], curOrient);
+	//}
+	////For complexity Calc
+	//maxGraphSize = std::max<size_t>(maxGraphSize, Graph.size());
+	////--------------
+
+	//ActionQue.push(CLIMB);
+}
+
+Orientation AstarAlgo::calcOrient(const Node& source, const Node& target) {
+	if (source.x == target.x) {
+		if (target.y == source.y + 1) {
+			return RIGHT;
+		}
+		else if (target.y == source.y - 1) {
+			return LEFT;
+		}
+		else {
+			throw std::invalid_argument("Not valid movement");
+		}
+	}
+	else if (source.y == target.y){
+		if (target.x == source.x + 1) {
+			return UP;
+		}
+		else if (target.x == source.x - 1) {
+			return DOWN;
+		}
+		else {
+			throw std::invalid_argument("Not valid movemnet");
+		}
+	}
+	else {
+		throw std::invalid_argument("Not valid movement");
+	}
+}
+
+AstarAlgo::AstarAlgo() {
+
+}
+
+int Heuristic::MDistance(const AstarAlgo::MapData& mapdata, const AstarAlgo::NodeData& graph,
+	const AstarAlgo::Node& coord, const Coord2& dest) {
+	return std::abs(dest.x - coord.x) + std::abs(dest.y - coord.y);
+}
+
+int Heuristic::CostBase(const AstarAlgo::MapData& mapdata, const AstarAlgo::NodeData& graph,
+	const AstarAlgo::Node& coord, const Coord2& dest) {
+	int result = 0;
+	AstarAlgo::Node pNode = graph[coord.parentIndex];
+	result += Heuristic::MDistance(mapdata, graph, coord, dest);
+	Orientation newOr = AstarAlgo::calcOrient(pNode, coord);
+	switch (AstarAlgo::table[pNode.or * 4 + newOr]) {
+	case AstarAlgo::mOrient::N:
+		break;
+	case AstarAlgo::mOrient::U:
+		result += 2;
+		break;
+	case AstarAlgo::mOrient::L:
+	case AstarAlgo::mOrient::R:
+		result += 1;
+		break;
+	}
+	return result;
+}
+
 #pragma endregion
--- WumpusWorldSimulator/Agent.h
+++ WumpusWorldSimulator/Agent.h
@@ -8,8 +8,11 @@
 #include "Orientation.h"
 #include <array>
 #include <deque>
+#include <queue>
 #include <vector>
 #include <functional>
+
+class AstarAlgo;
 
 struct Coord2 {
 	int x, y;
@@ -46,7 +49,58 @@ public:
 	void GetPercept(const Coord2& coord, Percept p);
 };
 
+class AstarAlgo{
+public:
+	typedef Knowledge::_mapdata MapData;
+	enum mOrient { N, R, L, U };
+	const long long upperBound = 2147483647;
+	struct Node{
+		int x;
+		int y;
+		int parentIndex, index;
+		Orientation or;
+		long long cost;
+		Node() {
+			x = -1;
+			y = -1;
+			parentIndex = -1;
+			index = -1;
+			cost = -1;
+			or = RIGHT;
+		}
+		Node(const Node& obj) {
+			x = obj.x;
+			y = obj.y;
+			parentIndex = obj.parentIndex;
+			index = obj.index;
+			cost = obj.cost;
+			or = obj.or;
+		}
+	};
+	static mOrient table[16];
+	Coord2 Origin;
+	Coord2 Dest;
 
+
+	//Variable for a*algorithm
+	typedef std::vector<Node> NodeData;
+
+	NodeData Graph;
+	NodeData Candidates;
+	std::vector<std::vector<bool>> isAccessed;
+	//functions-------
+	std::queue<Action> ActionQue;
+	void Compute(const MapData& mapdata, Coord2 start, Coord2 target, Orientation initorient);
+	void Compute(const MapData& mapdata, const Node& curNode, Coord2 target);
+	void MakeAction(const MapData& mapdata);
+	//Scoring Data
+	int funcCallNum; //Time
+	size_t maxGraphSize; //Space
+	//----------------
+	std::deque<Action> operator() (const AstarAlgo::MapData& mapdata, const Coord2& origin, const Coord2& dest, Orientation initori);
+	static Orientation calcOrient(const Node& source, const Node& target);
+	AstarAlgo();
+};
 
 class Agent {
 private:
@@ -58,17 +112,22 @@ private:
 	std::deque<Action> actionQueue;
 
 	Coord2 FindNextDest();
-	_coordlist FindPath(const Coord2& dest);
-	void MakeActionQueue(_coordlist& path);
+	AstarAlgo algoMethod;
 public:
 	const static int defaultSize = 5;
 
-	Agent ();
-	~Agent ();
-	void Initialize ();
+	Agent();
+	~Agent();
+	void Initialize();
 	void Initialize(int worldSize, MapState* map);
-	Action Process (Percept& percept);
-	void GameOver (int score);
+	Action Process(Percept& percept);
+	void GameOver(int score);
+};
+
+namespace Heuristic {
+	//int(const Mapdata&, pair<int, int>)
+	int MDistance(const AstarAlgo::MapData& mapdata, const AstarAlgo::NodeData& graph, const AstarAlgo::Node& coord, const Coord2& dest);
+	int CostBase(const AstarAlgo::MapData& mapdata, const AstarAlgo::NodeData& graph, const AstarAlgo::Node& coord, const Coord2& dest);
 };
 
 #endif // AGENT_H
